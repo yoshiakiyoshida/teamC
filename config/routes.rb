d2883dfdd02c96e69a15c10b39ebaedc0e3dc564
吉田　喜昭 <>
2021-02-15 19:06:11 +0900

    ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  # ログイン
+  resource :session, only:[:new :create, :destroy]
+  # パスワード再設定
+  resource :account, expect:[:new, :create, :destroy]
+  # メニュー
+  root "top#index"
+  # 社員情報各画面
+  resources :syain
+  # 検索画面
+  get "search" => "search#index"
+
 end
